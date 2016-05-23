353bc75017c1169d953e50f952e2c5badd8d1691
codeki <codeki@server.fake>
2016-05-22 22:28:43 -0700

    added smoothing and export settings to clear UE4 import errors

--- makeBook.py
+++ makeBook.py
@@ -29,8 +29,10 @@ while True:
 
 	bpy.ops.mesh.primitive_cube_add()
 	bpy.ops.transform.resize(value=(d,w,h))
+	bpy.ops.object.shade_smooth()
 
-	bpy.ops.export_scene.fbx(filepath="./"+title+".fbx",use_selection=True)
+
+	bpy.ops.export_scene.fbx(filepath="./"+title+".fbx",use_selection=True,global_scale=1.0, axis_forward='-Z', axis_up='Y', object_types={'MESH'}, use_mesh_modifiers=True, mesh_smooth_type='FACE', use_mesh_edges=False, use_anim=True, use_anim_action_all=True, use_default_take=True, use_anim_optimize=True, anim_optimize_precision=6.0, path_mode='AUTO', batch_mode='OFF', use_batch_own_dir=True, use_metadata=True)
 
 
 f.close()
